f0432f81df3869601bf72760ede1e57020c526c9
Lonirs Hervilla <lhervi@gmail.com>
2025-09-03 10:06:45 -0400

    update - probada todas las rutas

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -15,12 +15,13 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", homeHandler).Methods("GET")
+	r.HandleFunc("/posts", blog.GetAllPosts).Methods("GET") // <-- Esta es la que se está llamando
+	r.HandleFunc("/posts/{id}", blog.GetPostsHandler).Methods("GET")
 
-	//r.HandleFunc("/posts", blog.CreatePostHandler).Methods("GET")
-	//r.HandleFunc("/posts", blog.GetPostsHandler).Methods("POST")
-
-	r.HandleFunc("/posts", blog.GetPostsHandler).Methods("GET")    // <-- Esta es la que se está llamando
 	r.HandleFunc("/posts", blog.CreatePostHandler).Methods("POST") // <-- Esta es la que se debería llamar
+
+	r.HandleFunc("/posts/{id}", blog.UpdatePostHandler).Methods("PUT")
+	r.HandleFunc("/posts/{id}", blog.DeletePostHandler).Methods("DELETE")
 
 	fmt.Println("servidor escuchando por el puerto 8000...")
 	log.Fatal(http.ListenAndServe(":8000", r))
--- internal/blog/handler.go
+++ internal/blog/handler.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
-var posts = []Post{}
+//var posts = []Post{}
+
+var posts = make(map[string]Post)
 
 func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
 	var newPost Post
@@ -23,15 +27,74 @@ func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
 	newPost.CreatedAt = time.Now()
 
 	//Añadir el nuevo post a la lista
-	posts = append(posts, newPost)
+	//posts = append(posts, newPost)
+
+	posts[newPost.ID] = newPost
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newPost)
 }
 
 func GetPostsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	postsID := vars["id"]
+
+	post, ok := posts[postsID]
+	if !ok {
+		http.Error(w, "Post no encontrado", http.StatusNotFound)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	//w.Write([]byte("Lista de Post"))
+	json.NewEncoder(w).Encode(post)
+}
+
+func GetAllPosts(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(posts)
+}
+
+func UpdatePostHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	postID := vars["id"]
+
+	var updatedPost Post
+	err := json.NewDecoder(r.Body).Decode(&updatedPost)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, ok := posts[postID]
+	if !ok {
+		http.Error(w, "Post no encontrado", http.StatusNotFound)
+		return
+	}
+
+	// Mantener el ID original y la fecha de creación
+	updatedPost.ID = postID
+	updatedPost.CreatedAt = posts[postID].CreatedAt
+	posts[postID] = updatedPost
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedPost)
+
+}
+
+func DeletePostHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	postID := vars["id"]
+
+	_, ok := posts[postID]
+	if !ok {
+		http.Error(w, "Post no encontrado", http.StatusNotFound)
+		return
+	}
+
+	delete(posts, postID)
+	w.WriteHeader(http.StatusNoContent)
 }
